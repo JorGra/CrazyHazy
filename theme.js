ba04626ab7973b013e8c478f41d9e2e07555ecf2
Joris <Joris.grau2@gmail.com>
2023-08-30 15:29:46 +0200

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,25 +1,12 @@
-/*
 (() => {
-    const script = document.createElement("SCRIPT");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute(
-      "src",
-      "https://astromations.github.io/Hazy/hazy.js"
-    );
-    document.head.appendChild(script);
-  })();
-*/
-
-
-  (() => {
-    const script = document.createElement("SCRIPT");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute(
-      "src",
-      "https://jorgra.github.io/CrazyHazy/theme_crazy_hazy.js"
-    );
-    document.head.appendChild(script);
-  })();
+  const script = document.createElement("SCRIPT");
+  script.setAttribute("type", "text/javascript");
+  script.setAttribute(
+    "src",
+    "https://jorgra.github.io/CrazyHazy/theme_crazy_hazy.js"
+  );
+  document.head.appendChild(script);
+})();
 
 
   
